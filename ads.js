577a80a79ab35f8fc9f4670558cafa3cafbbfb8e
modora-official <shidaytl23032003@gmail.com>
2026-03-24 03:25:07 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -2,3 +2,4 @@
 (function(s){s.dataset.zone='10765319',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))
 
 // ads 2
+(function(s){s.dataset.zone='10765288',s.src='https://nap5k.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))
