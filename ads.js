cb9c8d8405e437493cfd6bebc1b18430055b7189
modora-official <shidaytl23032003@gmail.com>
2026-03-24 04:05:27 +0700

    Delete ads.js

--- ads.js
+++
@@ -1,8 +0,0 @@
-// ads 1
-(function(s){s.dataset.zone='10765319',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')));
-
-// ads 2
-(function(s){s.dataset.zone='10765288',s.src='https://nap5k.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')));
-
-// ads 3
-(function(s){s.dataset.zone='10765281',s.src='https://izcle.com/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')));
