ddc4b81a22d101921ad3e7a16dcef84ebf205725
modora-official <shidaytl23032003@gmail.com>
2026-03-28 20:09:15 +0700

    MODORA Official

---
+++ ads.js
@@ -0,0 +1,18 @@
+// <script src="../../../ada.js"></script>
+// Iklan Klik
+(function(s){
+    s.dataset.zone='10765319';
+    s.src='https://al5sm.com/tag.min.js';
+})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')));
+
+// Iklan Banner 1
+(function(s){
+    s.dataset.zone='10777339';
+    s.src='https://nap5k.com/tag.min.js';
+})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')));
+
+// Iklan Banner 2
+(function(s){
+    s.dataset.zone='10765281';
+    s.src='https://izcle.com/vignette.min.js';
+})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')));
