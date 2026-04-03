3174929a0d91ca71f19d44e71510a8028ae9cfeb
modora-official <shidaytl23032003@gmail.com>
2026-04-03 23:41:42 +0700

    MODORA Official

---
+++ short.js
@@ -0,0 +1,9 @@
+// Deklarasi variabel SafeLinkU
+var go_url = 'https://sfl.gl/'; 
+var api = 'c6860d49e9dc09f506140805a1cd075cdc10f23b'; 
+var shorten_includ = ["safefileku.com"];
+
+// Memuat script eksternal web-script.js secara dinamis
+var script = document.createElement('script');
+script.src = '//safelinku.com/js/web-script.js';
+document.head.appendChild(script);
