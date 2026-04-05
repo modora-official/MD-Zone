12c3811e32fcae9c05947f58a9c1d70b05529c00
modora-official <shidaytl23032003@gmail.com>
2026-04-05 19:20:04 +0700

    Update and rename ads to ads.js

---
+++ ads.js
@@ -0,0 +1,10 @@
+// <script src="../../../ada.js"></script>
+// Iklan Klik
+// ads.js
+
+(function() {
+    var script = document.createElement('script');
+    script.src = "https://pl29049211.profitablecpmratenetwork.com/67/b1/9b/67b19bcda13dcfd3eea3c1a2a8f3920a.js";
+    script.async = true;
+    document.head.appendChild(script);
+})();
