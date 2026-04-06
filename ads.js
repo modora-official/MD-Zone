d18c22c774302133b28311caab0894a628a6f50f
modora-official <shidaytl23032003@gmail.com>
2026-04-06 10:20:23 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -2,12 +2,30 @@
 
 
 
+// --- 1. Kode Iklan Popunder ---
 (function(pyuhdti){
 var d = document,
     s = d.createElement('script'),
     l = d.scripts[d.scripts.length - 1];
 s.settings = pyuhdti || {};
 s.src = "\/\/limited-teacher.com\/cBDU9.6\/bB2\/5TloSYWYQ\/9\/NGjMk\/z-MyjvIe4oObSR0\/2dOMTtMjyoMYjGk\/zf";
+s.async = true;
+s.referrerPolicy = 'no-referrer-when-downgrade';
+if (l && l.parentNode) {
+    l.parentNode.insertBefore(s, l);
+} else {
+    d.head.appendChild(s);
+}
+})({});
+
+
+// --- 2. Kode Iklan Video ---
+(function(blqj){
+var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = blqj || {};
+s.src = "\/\/superbjudgment.com\/bUXnV.sud\/Gylq0hYkWBcF\/Se\/mY9YuLZwUDlVknPWTVY\/5IMjz\/IXz\/NNDCUvtoN\/jPkezMMkj_Mn0ROrQl";
 s.async = true;
 s.referrerPolicy = 'no-referrer-when-downgrade';
 if (l && l.parentNode) {
