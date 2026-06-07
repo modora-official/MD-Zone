e0438a7d805ba8b1bd9742be29e7f71968702cbb
modora-official <shidaytl23032003@gmail.com>
2026-06-07 23:18:48 +0700

    Delete short.js v2.0

--- short.js
+++
@@ -1,9 +0,0 @@
-//safelinku
-var go_url = 'https://sfl.gl/';
-var api = 'c6860d49e9dc09f506140805a1cd075cdc10f23b';
-var shorten_includ = ["safefileku.com"];
-
-// load script eksternal
-var script = document.createElement('script');
-script.src = "//safelinku.com/js/web-script.js";
-document.body.appendChild(script);
